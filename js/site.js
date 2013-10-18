11718dab0a89f0bb37c35a91ffe7a78a27ad1f29
deeve007 <dave69@gmail.com>
2013-10-18 13:09:29 -0700

    Updated mobile menu code

--- js/site.js
+++ js/site.js
@@ -8,10 +8,11 @@
 		$(function() {
 		    $('#togglebutt').click(function() {
 				$('nav[role="navigation"]').slideToggle('slow');
+				return false;
 			});
 		});
 		
-		// if window is resized and broswer changes from mobile to desktop, need to be sure to display desktop even uf user has hidden via above
+		// if window is resized and browser changes from mobile to desktop, need to be sure to display desktop even uf user has hidden via above
 		$(window).resize(function(){
 			var w = $(window).width();
 			if(w >= 720 && $('nav[role="navigation"]').is(':hidden')) {
--- parts/shared/header.php
+++ parts/shared/header.php
@@ -4,7 +4,7 @@
 		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 		<h2><?php bloginfo( 'description' ); ?></h2>
 		
-		<button id="togglebutt">Show/hide menu</button><!-- for mobile menu -->
+		<a href="#" id="togglebutt">Show/hide menu</a><!-- for mobile menu -->
 		<nav id="primarynav" role="navigation">
 			<?php wp_nav_menu( array('menu' => 'Primary', 'container' => false, 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>', )); ?>
 		</nav>
